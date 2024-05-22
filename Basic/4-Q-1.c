3512f6138ff8f27d25457e8495c1bbcf024a3692
Asem Hamid <155321064+asem-hamid@users.noreply.github.com>
2024-05-22 16:28:07 +0600

    Create 4-Q-1.c

---
+++ Basic/4-Q-1.c
@@ -0,0 +1,19 @@
+// What is the output of following c program
+
+# include <stdio.h>
+
+int main(){
+
+printf("%d", printf("%s","Hello world"));
+
+return 0;
+}
+
+//Ans ) Hello world11
+
+
+// %s is used to print "string of characters?"
+/*
+Printf not only prints the content on the screen. It also
+returns the number of characters that it successfully prints on the screen.
+*/
