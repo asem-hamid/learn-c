66ee3a6b22d85a3cb96b108fc58e380411de264f
Asem Hamid <155321064+asem-hamid@users.noreply.github.com>
2024-05-22 20:29:21 +0600

    Update 2.1-extern.c

--- Basic/4-modifier/2.1-extern.c
+++ Basic/4-modifier/2.1-extern.c
@@ -14,3 +14,12 @@ printf("%d", myExternvar);
 return 0;
 }
 
+/*
+
+1. When we write extern some_data_type some_variable_name; no memory is allocated. Only property of variable is announced.
+2. Multiple declarations of extern variable is allowed within the file. This is not the case with automatic variables.
+3. Extern variable says to compiler "go outside from my scope and you will find the definition of the variable that I declared".
+4. Compiler believes that whatever the extern variable said is true and produce no error. Linker throws an error when he finds no such variable exist.
+5. When an extern variable is initialized, then memory for this variable is allocated and it will be considered defined.
+
+*/
