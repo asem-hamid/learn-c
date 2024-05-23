d3f69e3d63a75d38a52d90b9d802d6fd0ad975d1
Asem Hamid <155321064+asem-hamid@users.noreply.github.com>
2024-05-23 12:26:24 +0600

    Update 4.2-static.c

--- 1-basic/4-modifier/4.2-static.c
+++ 1-basic/4-modifier/4.2-static.c
@@ -19,3 +19,16 @@ printf("\n%d",function()); // 3
   
 return 0;
 }
+
+/*
+
+1. Static variable remains in memory even if it is
+   declared within a block on the other hand automatic 
+   variable is destroyed after the completion of function 
+   in which it was declared. 
+
+2. Static variable if declared outside the scope of any 
+   function will act like global variable but only within 
+   the file in which it is declared.
+
+*/
