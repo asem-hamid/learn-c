dbca93893dc5f418ad2c69db3a84cbef3e8fd37b
Asem Hamid <155321064+asem-hamid@users.noreply.github.com>
2024-05-23 13:03:33 +0600

    Create 1.3-define.c

---
+++ 1-basic/5-constants/1.3-define.c
@@ -0,0 +1,15 @@
+// We can write multiple lines using \
+
+#include <stdio.h>
+
+#define greater (x, y) if(x > y) \
+                       printf("%d is greater than %d", x, y); \
+                       else \
+                       printf("%d is lesser than %d", x, y);
+
+int main() {
+  
+greater (5, 6);
+  
+return 0;
+}
