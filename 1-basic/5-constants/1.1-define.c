7bb335918c344c9ae8cff1bffaf35c0e2e82d15f
Asem Hamid <155321064+asem-hamid@users.noreply.github.com>
2024-05-23 13:03:22 +0600

    Create 1.1-define.c

---
+++ 1-basic/5-constants/1.1-define.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+#define PI 3.1416  // we don't have to use ' ; ' at the end
+
+// here PI ( or other name ) also called Macro 
+
+int main(){
+
+printf("%f\n",PI);
+
+return 0;
+}
+
+// Choosing capital letter for Macro is good practice
