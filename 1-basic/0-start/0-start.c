b41f6d1adfc8ad04c34c94b30c3c943aef099503
Asem Hamid <155321064+asem-hamid@users.noreply.github.com>
2024-05-23 13:28:18 +0600

    Update 0-start.c

--- 1-basic/0-start/0-start.c
+++ 1-basic/0-start/0-start.c
@@ -1,20 +1,20 @@
-# include<stdio.h>
+# include <stdio.h>
 
 /*this is my first programme*/
 int main() {
-   int age = 20 ;
-    float hight = 5.5;
-    char email = '@';
+   int age = 20 ; // int datatype
+   float hight = 5.5; // float datatype
+   char email = '@'; // char datatype
 
-    printf("hello world\n");
-    printf("My name is Asem\n");
+    printf("hello world\n");       // printf function to print any thing
+    printf("My name is Asem\n");   // '  \n  '  for next line
     printf("my age is %d\n",age);
     printf("my hight is %f\n",hight);
     printf("Sign use in email is %c\n",email);
    
    int age2 ;
     printf("What is your age?\n");
-    scanf("%d",&age2);
+    scanf("%d",&age2);    // scanf function to take input from user 
     printf("P2: my age is %d\n",age2); 
     printf("nice . give me 2 numbers \n"); 
 
@@ -33,10 +33,10 @@ int main() {
 
  printf("Hello World!\nI am learning C.\nAnd it is awesome!\n");
 
- printf("Hello World!\n\n");
+ printf("Hello World!\n\n"); 
  printf("I am learning C.\n");
 
- printf("Hello World!\t");
+ printf("Hello World!\t"); 
  printf("I am learning C.\n");
 
  printf("Hello World!\\");
