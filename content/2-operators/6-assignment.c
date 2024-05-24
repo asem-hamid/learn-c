0e90de7ddbb51967ecbf3c0c6f5dbda235f2a97e
Asem Hamid <155321064+asem-hamid@users.noreply.github.com>
2024-05-24 23:59:43 +0600

    Create 6-assignment.c

---
+++ content/2-operators/6-assignment.c
@@ -0,0 +1,43 @@
+﻿# include <stdio.h>
+
+int main(){
+
+// +=  First addition than assignment
+// -=  First subtraction than assignment
+// *=  First multiplication than assignment
+// /=  First division than assignment
+// %=  First modulus than assignment
+// <<=  First bitwise left shift than assignment
+// >>=  First bitwise right shift than assignment
+// &=  First bitwise AND than assignment
+// |=  First bitwise OR than assignment
+// Λ=  First bitwise XOR than assignment
+
+
+int a = 5;
+int b = 4;
+int c = 3;
+
+a += b ; // a = a+b 
+printf("%d\n",a); // 9
+a -= c ; // a = a-c
+printf("%d\n",a); // 6
+a *= b ; // a = a*b 
+printf("%d\n",a); // 24
+a /= c ; // a = a/c 
+printf("%d\n",a); // 8
+a %= c ; // a = a%c
+printf("%d\n",a); // 2
+a <<= c ; // a = a<<c
+printf("%d\n",a); // 16
+a >>= c ; // a = a>>c
+printf("%d\n",a); // 2
+a &= c ; // a = a&c
+printf("%d\n",a); // 2
+a |= c ; // a = a|c
+printf("%d\n",a); // 3
+a ^= c ; // a = a6c
+printf("%d\n",a); // 0
+
+return 0;
+}
