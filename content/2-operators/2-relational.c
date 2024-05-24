6515b091e3546c30262a7d554ab8ad099360ab91
Asem Hamid <155321064+asem-hamid@users.noreply.github.com>
2024-05-24 17:05:51 +0600

    Create 2-relational.c

---
+++ content/2-operators/2-relational.c
@@ -0,0 +1,27 @@
+# include <stdio.h>
+
+int main(){
+printf("Relational Operators   \n");
+
+
+
+printf(" if it print 1 it is ture and if it print 0 it is false\n");
+
+
+printf("%d\n", 4==4);
+printf("%d\n", 5==7);
+printf("%d\n", 5!=6);
+printf("%d\n", 3!=3);
+
+printf("%d\n", 4>2);
+printf("%d\n", 4>4);
+printf("%d\n", 4>=4);
+printf("%d\n", 4>=6);
+
+printf("%d\n", 3<5);
+printf("%d\n", 3<2);
+printf("%d\n", 3<=3);
+printf("%d\n", 3<=2);
+  
+return 0;
+}
