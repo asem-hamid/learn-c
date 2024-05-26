14ddd1886cc712f14ccbdcd151052527703cdd24
Asem Hamid <155321064+asem-hamid@users.noreply.github.com>
2024-05-26 13:01:12 +0600

    Create 3-do-while.c

---
+++ content/4-loop/3-do-while.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+ printf("do while loop\n");
+
+  printf("new loop \n");
+
+  int j=5;
+
+  do{
+
+    printf("loop no %d \n", j);
+    j--;
+  }while(j>=1);
+
+
+
+return 0;
+}
