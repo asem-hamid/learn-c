18b33df250964f8bbf06ee023c95d335fd8f0b51
Asem Hamid <155321064+asem-hamid@users.noreply.github.com>
2024-05-26 12:58:40 +0600

    Create 2-while.c

---
+++ content/4-loop/2-while.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+ printf("while loop\n");
+
+  printf("new loop \n");
+
+  int i=1;
+
+  while(i<=10){
+
+    printf("loop no %d\n", i);
+    i++;
+  }
+
+return 0;
+}
