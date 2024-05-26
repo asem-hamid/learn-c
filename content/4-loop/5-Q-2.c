3928aa4f934afdcc0b7f6a80dcc31ead4f0e236d
Asem Hamid <155321064+asem-hamid@users.noreply.github.com>
2024-05-26 13:06:28 +0600

    Create 5-Q-2.c

---
+++ content/4-loop/5-Q-2.c
@@ -0,0 +1,19 @@
+// table of n
+
+#include <stdio.h>
+
+int main() {
+
+   
+   int table; 
+ 
+   printf("The table of %d is \n", n);
+
+   for(int j = 1;j <=10;j++){
+    table = n * j;
+    printf("%d * %d = %d \n", n, j, table);
+
+   }
+
+return 0;
+}
