fe39d45e5d1d94c79972a05990776e9fd608c6ea
Asem Hamid <155321064+asem-hamid@users.noreply.github.com>
2024-05-26 12:26:45 +0600

    Create 4-switch.c

---
+++ content/3-conditional/4-switch.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+
+int main() {
+
+switch(daynumber) {
+   case 1 : 
+    printf("saturday \n");
+   
+
+    switch(time) {
+       case 12 : 
+        printf("it is noon\n");
+        break;
+       case 3 : 
+        printf("it is afternoon\n");
+        break;
+       case 6 :
+        printf("it is evening\n");
+        break;
+       case 9 : 
+        printf("it is night\n");
+        break;
+    }
+    break;
+   case 2 :
+    printf("sunday \n");
+
+     switch(time) {
+       case 12 : 
+        printf("it is noon\n");
+        break;
+       case 3 : 
+        printf("it is afternoon\n");
+        break;
+       case 6 :
+        printf("it is evening\n");
+        break;
+       case 9 : 
+        printf("it is night\n");
+        break;
+     }
+    break;
+   case 3 :
+    printf("monday \n");
+
+    switch(time) {
+       case 12 : 
+        printf("it is noon\n");
+        break;
+       case 3 : 
+        printf("it is afternoon\n");
+        break;
+       case 6 :
+        printf("it is evening\n");
+        break;
+       case 9 : 
+        printf("it is night\n");
+        break;
+     }
+    break;
+   case 4 :
+    printf("tuesday \n");
+
+    switch(time) {
+       case 12 : 
+        printf("it is noon\n");
+        break;
+       case 3 : 
+        printf("it is afternoon\n");
+        break;
+       case 6 :
+        printf("it is evening\n");
+        break;
+       case 9 : 
+        printf("it is night\n");
+        break;
+     }
+    break;
+   case 5 :
+    printf("wednesday \n");
+
+    switch(time) {
+       case 12 : 
+        printf("it is noon\n");
+        break;
+       case 3 : 
+        printf("it is afternoon\n");
+        break;
+       case 6 :
+        printf("it is evening\n");
+        break;
+       case 9 : 
+        printf("it is night\n");
+        break;
+     }
+    break;
+   case 6 :
+    printf("thursday \n");
+
+    switch(time) {
+       case 12 : 
+        printf("it is noon\n");
+        break;
+       case 3 : 
+        printf("it is afternoon\n");
+        break;
+       case 6 :
+        printf("it is evening\n");
+        break;
+       case 9 : 
+        printf("it is night\n");
+        break;
+     }
+    break;
+   case 7 :
+    printf("friday \n");
+
+    switch(time) {
+       case 12 : 
+        printf("it is noon\n");
+        break;
+       case 3 : 
+        printf("it is afternoon\n");
+        break;
+       case 6 :
+        printf("it is evening\n");
+        break;
+       case 9 : 
+        printf("it is night\n");
+        break;
+     }
+    break;
+   default :
+    printf("invalid number \n"); 
+
+} 
+
+
+return 0;
+}
