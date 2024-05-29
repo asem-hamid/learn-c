78c94712de8f4cfd3c610a5d90be6310f0463ac1
Asem Hamid <155321064+asem-hamid@users.noreply.github.com>
2024-05-29 13:30:09 +0600

    Create 7-Q-2.c

---
+++ content/5-function/7-Q-2.c
@@ -0,0 +1,34 @@
+﻿// What tis the output of the following C program is:
+
+# include <stdio.h>
+
+
+void f1(int a, int b);
+
+void f2(int *a, int *b);
+
+int main()
+{
+int a=4, b=5, c=6;
+
+f1(a, b);
+f2(&b, &c);
+
+printf("%d", c-a-b);
+
+return 0;
+}
+
+
+
+void f1(int a, int b){
+
+int c;
+c = a; a = b; b = c;
+}
+
+void f2(int *a, int *b){
+
+int c;
+c = *a; *a = *b; *b = c;
+}
