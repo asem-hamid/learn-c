e3a93fd028cb398ec966b897bec03a73c83cbceb
Asem Hamid <155321064+asem-hamid@users.noreply.github.com>
2024-05-29 12:43:36 +0600

    Create 1-function-1.c

---
+++ content/5-function/1-function-1.c
@@ -0,0 +1,32 @@
+# include <stdio.h>
+
+void printhello();
+void printbonjour();
+void printgoodbye();
+
+int main(){
+
+int c;
+printf("Where are you from? \n");
+printf("1- England 2- French \n");
+scanf("%d",&c);
+
+if(c == 1){
+
+    printhello();
+    
+}
+
+else if(c == 2){
+
+    printbonjour();
+
+}
+else{
+
+    printgoodbye();
+    
+}
+
+return 0;
+}
