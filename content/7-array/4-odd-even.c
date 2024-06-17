458a5b825c1ac537d9162abb34de2e7cc3cf14ee
Asem Hamid <155321064+asem-hamid@users.noreply.github.com>
2024-06-17 13:49:39 +0600

    Create 4-odd-even.c

---
+++ content/7-array/4-odd-even.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void how_many_odd (int Oarr[] ,int *oddnum,int *evennum);
+
+int main() {
+
+
+int oddnum = 0;
+int evennum = 0;
+int Oarr[] = {1,2,3,4,5,6,7};
+ 
+how_many_odd (Oarr , &oddnum,&evennum);
+
+printf("odd number = %d\n",oddnum);
+printf("even number = %d\n",evennum);
+
+
+
+    return 0;
+}
+
+
+void how_many_odd (int Oarr[] ,int *oddnum,int *evennum){
+      
+      
+    for(int i = 0;i < 7;i++){
+
+        if(Oarr[i] % 2 != 0){
+
+           (*oddnum)++;
+        }
+        else if(Oarr[i] % 2 == 0){
+
+           (*evennum)++;
+        }
+   
+    }
+
+}
