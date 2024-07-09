26b1f8cb516bd5e74f52c6421f8250f8fc6f41c6
Asem Hamid <155321064+asem-hamid@users.noreply.github.com>
2024-07-09 13:31:45 +0600

    Create 2-arr-of-struct.c

---
+++ content/-10-structures/2-arr-of-struct.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+
+struct student{
+
+  char name[100];
+  int  roll;
+  int  cgpa;
+};
+
+int main() {
+
+// Array of Structures
+ 
+    struct student cse[100];
+    struct student eee[100];
+    struct student it [100];
+
+    cse[0].roll = 39;
+    cse[0].cgpa = 3.28;
+    strcpy(cse[0].name , "rahim");
+
+
+    printf("roll is %d\n",cse[0].roll);
+    printf("cgpa is %f\n",cse[0].cgpa);
+    printf("name is %s\n",cse[0].name);
+
+    cse[1]= (struct student){"fahim" ,56 , 3.75};// We have to use (struct student) to assign in a single line in array
+
+
+    printf("roll is %d\n",cse[1].roll);
+    printf("cgpa is %f\n",cse[1].cgpa);
+    printf("name is %s\n",cse[1].name);
+
+
+    eee[0] = (struct student){"rahim",29 ,3.83};
+
+    printf("roll is %d\n",eee[0].roll);
+    printf("cgpa is %f\n",eee[0].cgpa);
+    printf("name is %s\n",eee[0].name);
+
+
+return 0;
+}
+
+
+
+
